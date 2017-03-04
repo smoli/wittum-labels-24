4ec85a0a6708aef1a9e74a7763d4a89227e74921
Stephan Smola <smola@farbspender.de>
2017-03-04 23:06:33 +0100

    switched to SVG because of Safari

--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,105 @@
 import React, {Component} from 'react';
 import {observer} from 'mobx-react';
 
-var Spacer = props => <div style={{height: "2cm"}}></div>;
+var Spacer = props => <div style={{height: "1cm"}}></div>;
 
 var Box = props => {
-    var solidLine = "1pt solid black";
-    var dottedLine = "1pt solid #eee";
-    var invisibleLine = "1pt solid white";
-    var style = {
-        display: "table-cell",
-        width: props.w + "mm",
-        maxWidth: props.w + "mm",
-        overflow: "hidden",
-        height: props.h + "mm",
-        maxHeight: props.h + "mm",
-        padding: "0"
-    };
-
-    style.borderLeft = invisibleLine;
-    style.borderTop = invisibleLine;
-    style.borderBottom = invisibleLine;
-    style.borderRight = invisibleLine;
-
-    if (props.b) {
-        style.borderBottom = solidLine;
-    }
-
+    var d = "";
     if (props.t) {
-        style.borderTop = solidLine;
+        d += `M0 0 l${props.w} 0 `;
     }
-
-    if (props.l) {
-        style.borderLeft = solidLine;
-    }
-
     if (props.r) {
-        style.borderRight = solidLine;
+        d += `M${props.w} 0 l0 ${props.h} `;
+    }
+    if (props.b) {
+        d += `M0 ${props.h} l${props.w} 0 `;
+    }
+    if (props.l) {
+        d += `M0 0 l0 ${props.h} `;
     }
 
-    if (props.bd) {
-        style.borderBottom = dottedLine;
-    }
 
-    if (props.td) {
-        style.borderTop = dottedLine;
-    }
-
-    if (props.rd) {
-        style.borderRight = dottedLine;
-    }
-
-    if (props.ld) {
-        style.borderLeft = dottedLine;
-    }
-
-    return <div className={props.className} style={style}>{props.children}</div>
+    return <g transform={`translate(${props.x} ${props.y})`}>
+        <path d={d}/>
+    </g>;
 };
 
 var CutoutBox = props => {
     const leg = 5;
 
-    return <div className={props.className}>
-        <div>
-            <Box className="tl" w={leg} h={leg}/>
-            <Box className="tc" w={props.width} l r h={leg} bd={props.dotted}/>
-            <Box className="tr" w={leg} h={leg}/>
-        </div>
-        <div>
-            <Box className="cl" w={leg} h={props.height} t b rd={props.dotted}/>
-            <Box className="cc" w={props.width} h={props.height}>{props.children}</Box>
-            <Box className="cr" w={leg} h={props.height} t b ld={props.dotted}/>
-        </div>
-        <div>
-            <Box className="bl" w={leg} h={leg}/>
-            <Box className="bc" w={props.width} l r h={leg} td={props.dotted}/>
-            <Box className="br" w={leg} h={leg}/>
-        </div>
-    </div>
+    var w = 2 * leg + props.width;
+    var h = 2 * leg + props.height;
+
+    return <svg width={`${w}mm`} height={`${h}mm`} viewBox={`0 0 ${w} ${h}`}>
+        <Box w={leg} h={leg} r b x={0} y={0}/>
+        <Box w={leg} h={leg} l b x={leg + props.width} y={0}/>
+
+        <Box w={leg} h={leg} r t x={0} y={leg + props.height}/>
+        <Box w={leg} h={leg} l t x={props.width + leg} y={leg + props.height}/>
+
+        {props.dotted ?
+            <rect x={leg} y={leg} width={props.width} height={props.height} className="dotted"/>
+            : null
+        }
+
+        <g transform={`translate(${leg} ${leg})`}>
+            {props.children}
+        </g>
+        ;
+    </svg>;
 };
 
+
+@observer
+class Labels extends Component {
+    render() {
+        var appState = this.props.appState;
+
+        var appt = appState.showAppartment ? <span className="appartment">{appState.appartment}</span> : null;
+        var letterBoxText = appState.names.filter(n => !!n).join("/");
+        if (appState.showAppartment) {
+            letterBoxText = `${appState.appartment} - ${letterBoxText}`;
+        }
+
+        var lineSize = 6;
+        var doorBellText =
+            <text className="doorBell" x={47 / 2} y={appState.names.filter(n => !!n).length > 2 ? 0 : lineSize}>
+                {appState.names.map((n, i) => <tspan x={47 / 2} dy={lineSize}>{n}</tspan>)}
+            </text>;
+
+
+        return <div className="labelSheet">
+            <CutoutBox width={65} height={14}
+                       dotted={appState.dotted}>
+                <text className="letterBox" x={65 / 2} y={7}>{letterBoxText} </text>
+            </CutoutBox>
+            <Spacer/>
+            <CutoutBox width={65} height={14}
+                       dotted={appState.dotted}>
+                <text className="letterBox" x={65 / 2} y={7}>{letterBoxText} </text>
+            </CutoutBox>
+            <Spacer/>
+            <CutoutBox width={60} height={15}
+                       dotted={appState.dotted}>
+                <text className="letterBox" x={30} y={7.5}>{letterBoxText} </text>
+            </CutoutBox>
+            <Spacer/>
+            <CutoutBox width={47} height={37} dotted={appState.dotted}>
+                {doorBellText}
+                {appState.showAppartment ?
+                    <text className="doorBellAppartment" x="4" y="32">{appState.appartment}</text>
+                    : null
+                }
+            </CutoutBox>
+
+        </div>;
+    }
+}
 
 @observer
 class App extends Component {
     render() {
         var appState = this.props.appState;
-
-        var apts = [];
-        for (var i = 1; i <= 20; apts.push(i++)) {}
-
-        var appt = appState.showAppartment ? <span className="appartment">{appState.appartment}</span> : null;
 
         return (
             <div className="container">
@@ -100,7 +111,8 @@ class App extends Component {
                             {appState.names.map((n, i) =>
                                 <div className="form-group" key={i}>
                                     <label>{i ? "noch ein " : ""}Name</label><br/>
-                                    <input key={i} value={n} placeholder={i === 0 ? "ein Name pro Feld" : i > 2 ? "ab hier wird's eng" : `${i+1}. Name`}
+                                    <input key={`textField${i}`} value={n}
+                                           placeholder={i === 0 ? "ein Name pro Feld" : i > 2 ? "ab hier wird's eng" : `${i + 1}. Name`}
                                            onChange={e => this.nameChanged(n, i, e)}/></div>)}
 
                             <div className="checkbox">
@@ -110,16 +122,16 @@ class App extends Component {
                                     anzeigen
                                 </label>
                             </div>
-                                {appState.showAppartment ?
-                            <div className="form-group">
+                            {appState.showAppartment ?
+                                <div className="form-group">
                                     <label htmlFor="appartmentNumber">Wohnung&nbsp;</label>
-                                <select id="appartmentNumber" value={appState.appartment}
-                                        onChange={e => appState.appartment = e.target.value}>
-                                    {apts.map(i => <option key={i} value={i}>{i}</option>)}
-                                </select>
-                            </div>
-                                    :
-                                    null }
+                                    <select id="appartmentNumber" value={appState.appartment}
+                                            onChange={e => appState.appartment = e.target.value}>
+                                        {apts.map(i => <option key={i} value={i}>{i}</option>)}
+                                    </select>
+                                </div>
+                                :
+                                null }
 
                             <div className="checkbox">
                                 <label>
@@ -128,43 +140,26 @@ class App extends Component {
                                     Scheren
                                 </label>
                             </div>
-                            {appState.dotted ? <p className="info">Wenn du ein Messer und ein Lineal aus Metall hast, solltest du die Schnittkanten ausblenden.</p> : null}
+                            {appState.dotted ?
+                                <p className="info">Wenn du ein Messer und ein Lineal aus Metall hast, solltest du die
+                                    Schnittkanten ausblenden.</p> : null}
 
                             {appState.names.filter(n => !!n).length ?
-                            <div className="form-group">
-                                <button className="btn btn-success letsgo" onClick={() => { window.print(); return false; }}>Drucken</button>
-                            </div>
+                                <div className="form-group">
+                                    <button className="btn btn-success letsgo" onClick={() => {
+                                        window.print();
+                                        return false;
+                                    }}>Drucken
+                                    </button>
+                                </div>
                                 : null
                             }
-                        </form>
+                        </form>;
                     </div>
                     <div className="col-md-1 noprint"></div>
                     <div className="col-md-5">
                         <h1 className="noprint">Labels</h1>
-                        <div className="labelSheet">
-                            <CutoutBox className="letterBox" width="65" height="14"
-                                       dotted={appState.dotted}>
-                                {appt}
-                                <span className="names">{appState.names.filter(n => !!n).join("/")}</span></CutoutBox>
-                            <Spacer/>
-                            <CutoutBox className="letterBox" width="65" height="14"
-                                       dotted={appState.dotted}>
-                                {appt}
-                                <span className="names">{appState.names.filter(n => !!n).join("/")}</span></CutoutBox>
-                            <Spacer/>
-
-                            <CutoutBox className="letterBox" width="60" height="15"
-                                       dotted={appState.dotted}>
-                                {appt}
-                                <span className="names">{appState.names.filter(n => !!n).join("/")}</span></CutoutBox>
-                            <Spacer/>
-
-                            <CutoutBox className="doorBell" width="47" height="37"
-                                       dotted={appState.dotted}>
-                                {appt}
-                                { appState.names.filter(n => !!n).length < 3 ? <br/> : null}
-                                <span className="names">{appState.names.filter(n => !!n).map(n => [n, <br/>])}</span>
-                            </CutoutBox></div>
+                        <Labels appState={appState}/>
                     </div>
 
                 </div>
